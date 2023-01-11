65ee887c44ac0eba020791470c4463d4f6405f60
Andrew Chernigovsky <chernigovsky108@gmail.com>
2023-01-11 16:31:39 +0300

    strucruture3 - 3

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,44 +1,43 @@
-import './NavBar.scss';
+import "./NavBar.scss";
 import { useState } from "react";
-
 
 const NavBar = () => {
 	// const [isActive, setisActive] = useState(true);
 
 	let isActive = true;
-	
+
 	return (
 		<nav className="navbar">
 			<div className="navbar__container">
 				<ul className="navbar__list">
-					<li className={`navbar__item ${isActive ? 'active' : ''}`}>
+					<li className={`navbar__item ${isActive && "active"}`}>
 						<a href="#coldSnackes">Холодные закуски</a>
 					</li>
-					<li className={`navbar__item ${isActive ? 'active' : ''}`}>
+					<li className={`navbar__item ${isActive ? "active" : ""}`}>
 						<a href="#hotShackes">Горячие закуски</a>
 					</li>
-					<li className={`navbar__item ${isActive ? 'active' : ''}`}>
+					<li className={`navbar__item ${isActive ? "active" : ""}`}>
 						<a href="#meatDelicious">Мясные блюда</a>
 					</li>
-					<li className={`navbar__item ${isActive ? 'active' : ''}`}>
+					<li className={`navbar__item ${isActive ? "active" : ""}`}>
 						<a href="#soups">Супы</a>
 					</li>
-					<li className={`navbar__item ${isActive ? 'active' : ''}`}>
+					<li className={`navbar__item ${isActive ? "active" : ""}`}>
 						<a href="#fishDilicious">Рыбные блюда</a>
 					</li>
-					<li className={`navbar__item ${isActive ? 'active' : ''}`}>
+					<li className={`navbar__item ${isActive ? "active" : ""}`}>
 						<a href="#grillMenu">Гриль меню</a>
 					</li>
-					<li className={`navbar__item ${isActive ? 'active' : ''}`}>
+					<li className={`navbar__item ${isActive ? "active" : ""}`}>
 						<a href="#exsclusiveDelicious">Фирменные блюда</a>
 					</li>
-					<li className={`navbar__item ${isActive ? 'active' : ''}`}>
+					<li className={`navbar__item ${isActive ? "active" : ""}`}>
 						<a href="#drinks">Напитки</a>
 					</li>
 				</ul>
 			</div>
 		</nav>
 	);
-}
+};
 
 export default NavBar;
