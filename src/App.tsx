96696d3e49b6f081515b87aad034176392fb32f8
Andrew Chernigovsky <chernigovsky108@gmail.com>
2023-01-13 13:14:03 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.scss";
 import Header from "./layout/Header/Header";
 import Main from "./components/Main/Main";
 import { Routes, Route } from "react-router-dom";
-import NotFoundPage from "./pages/Notfoundpage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
 	return (
