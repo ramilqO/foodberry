8d651d6d5b0e5b490f49b717e68cd287bdcf24a6
Ramil' Osmanov <osmanovramil823@gmail.com>
2023-01-13 21:56:08 +0600

    footer section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from "./layout/Header/Header";
 import Main from "./components/Main/Main";
 import { Routes, Route } from "react-router-dom";
 import NotFoundPage from "./pages/NotFoundPage";
+import Footer from "./layout/Footer/Footer";
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 				<Route path="*" element={<NotFoundPage />} />
 			</Routes>
 			;
+			<Footer />
 		</div>
 	);
 }
