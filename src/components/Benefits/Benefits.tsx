64c67c53d8889c8ec5fa6091dc3c560298ef5641
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2023-01-22 21:53:11 +0300

    fix merging conflict

--- src/components/Benefits/Benefits.tsx
+++ src/components/Benefits/Benefits.tsx
@@ -1,45 +1,59 @@
-import { OnionIcon } from "../../icons/OnionIcon"
-import { FlashIcon } from "../../icons/FlashIcon"
-import { ChefIcon } from "../../icons/ChefIcon"
 import "./Benefits.scss";
+
 import { useState } from "react";
 
-const Benefits = () => {
-    const [isActive, setIsActive] = useState([false, false, false, false]);
+import { OnionIcon } from "../../icons/OnionIcon";
+import { FlashIcon } from "../../icons/FlashIcon";
+import { ChefIcon } from "../../icons/ChefIcon";
 
-    return (
-        <section className="benefits">
-            <div className="benefits__container">
-                <div className="descriptions">
-                    <div className="wrapper">
-                        <div className={"description-block " + (isActive[0] ? 'active' : '')}>
-                            <h2 className="description-block__title">НАШЕ КАФЕ0</h2>
-                            <p className="description-block__subtitle">Мы расположены в одном из самых живописных мест города — на берегу реки, это ваш оазис в черте города, куда можно сбежать от шумного и пыльного мегаполиса. Мы, действительно уникальные, ведь все продумано до мелочей: проект построен из дикого закарпатского сруба, камин в основном зале ресторана и панорамные окна с видом на реку, уютные беседки на берегу реки и лучшая видовая террасса, шатер с посадкой на 200 человек, сказочный детский домик и бассейн.</p>
-                        </div>
-                        <div className={"description-block " + (isActive[1] ? 'active' : '')}>
-                            <h2 className="description-block__title">НАШЕ КАФЕ1</h2>
-                            <p className="description-block__subtitle">Мы расположены в одном из самых живописных мест города — на берегу реки, это ваш оазис в черте города, куда можно сбежать от шумного и пыльного мегаполиса. Мы, действительно уникальные, ведь все продумано до мелочей: проект построен из дикого закарпатского сруба, камин в основном зале ресторана и панорамные окна с видом на реку, уютные беседки на берегу реки и лучшая видовая террасса, шатер с посадкой на 200 человек, сказочный детский домик и бассейн.</p>
-                        </div>
-                        <div className={"description-block " + (isActive[2] ? 'active' : '')}>
-                            <h2 className="description-block__title">НАШЕ КАФЕ2</h2>
-                            <p className="description-block__subtitle">Мы расположены в одном из самых живописных мест города — на берегу реки, это ваш оазис в черте города, куда можно сбежать от шумного и пыльного мегаполиса. Мы, действительно уникальные, ведь все продумано до мелочей: проект построен из дикого закарпатского сруба, камин в основном зале ресторана и панорамные окна с видом на реку, уютные беседки на берегу реки и лучшая видовая террасса, шатер с посадкой на 200 человек, сказочный детский домик и бассейн.</p>
-                        </div>
-                        <div className={"description-block " + (isActive[3] ? 'active' : '')}>
-                            <h2 className="description-block__title">НАШЕ КАФЕ3</h2>
-                            <p className="description-block__subtitle">Мы расположены в одном из самых живописных мест города — на берегу реки, это ваш оазис в черте города, куда можно сбежать от шумного и пыльного мегаполиса. Мы, действительно уникальные, ведь все продумано до мелочей: проект построен из дикого закарпатского сруба, камин в основном зале ресторана и панорамные окна с видом на реку, уютные беседки на берегу реки и лучшая видовая террасса, шатер с посадкой на 200 человек, сказочный детский домик и бассейн.</p>
-                        </div>
-                        <button className="descriptions__button">Посмотреть меню</button>
-                    </div>
-                    <div className="benefits__buttons">
-                        <button className="benefits__button" onClick={() => { setIsActive([true, false, false, false]) }}><OnionIcon /><span>Свежайшие продукты</span></button>
-                        <button className="benefits__button" onClick={() => { setIsActive([false, true, false, false]) }}><FlashIcon /><span>Быстрая доставка</span></button>
-                        <button className="benefits__button" onClick={() => { setIsActive([false, false, true, false]) }}><ChefIcon /><span>Лучшие повора</span></button>
-                        <button className="benefits__button" onClick={() => { setIsActive([false, false, false, true]) }}><OnionIcon /><span>Свежайшие продукты</span></button>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
-}
+import benefitsData from "./benefitsData";
+
+const Benefits = () => {
+	const [descriptionActive, setDescriptionActive] = useState({
+		title: benefitsData[0].title,
+		description: benefitsData[0].description,
+	});
+
+	return (
+		<section className="benefits">
+			<div className="benefits__container">
+				<div className="benefits__descriptions">
+					<div className="description-block active">
+						<h2 className="description-block__title">
+							{descriptionActive.title}
+						</h2>
+						<p className="description-block__subtitle">
+							{descriptionActive.description}
+						</p>
+					</div>
+
+					<button className="description-block__button">
+						Посмотреть меню
+					</button>
+				</div>
+
+				<div className="benefits__buttons">
+					{benefitsData.map((item) => {
+						return (
+							<button
+								key={item.button.name}
+								className="benefits__button"
+								onClick={() => {
+									setDescriptionActive({
+										title: item.title,
+										description: item.description,
+									});
+								}}
+							>
+								{item.button.name}
+							</button>
+						);
+					})}
+				</div>
+			</div>
+		</section>
+	);
+};
 
 export default Benefits;
+
--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,21 +1,21 @@
 import { food } from "../../dataBase";
 import "./Content.scss";
 
-import { Swiper, SwiperSlide} from 'swiper/react';
-import {Autoplay} from 'swiper';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
-import Card from '../Card/Card';
+import Card from "../Card/Card";
 import { InView } from "react-intersection-observer";
 
 interface IactiveSection {
 	setActiveSection: (item: string) => void;
 }
 
-const Content = ({setActiveSection}: IactiveSection) => {
+const Content = ({ setActiveSection }: IactiveSection) => {
 	return (
 		<section className="main-content">
 			<div className="main-content__container">
@@ -31,15 +31,34 @@ const Content = ({setActiveSection}: IactiveSection) => {
 								inView && setActiveSection(item.id)
 							}
 						>
-							{item.menuTitle}
+							<span className="menu-title">{item.menuTitle}</span>
 
 							<Swiper
 								modules={[Autoplay]}
 								spaceBetween={10}
-								slidesPerView={4}
-								className="12345"
-								style={{ maxWidth: "100%", display: "flex" }}
+								// slidesPerView={4}
+								className="main-content__slider"
 								autoplay={{ delay: 5000 }}
+								breakpoints={{
+									// when window width is >= 640px
+									640: {
+										width: 640,
+										slidesPerView: 1,
+									},
+									// when window width is >= 768px
+									768: {
+										width: 768,
+										slidesPerView: 2,
+									},
+									1024: {
+										width: 1024,
+										slidesPerView: 3,
+									},
+									1440: {
+										width: 1440,
+										slidesPerView: 4,
+									},
+								}}
 							>
 								{item.foods.map((dish, index) => {
 									return (
@@ -47,17 +66,13 @@ const Content = ({setActiveSection}: IactiveSection) => {
 											style={{ width: "auto" }}
 											key={index}
 										>
-											<span
-												style={{ background: "yellow" }}
-											>
-												<Card
-													img={dish.img}
-													name={dish.name}
-													weight={dish.weight}
-													price={dish.price}
-													about={dish.about}
-												/>
-											</span>
+											<Card
+												img={dish.img}
+												name={dish.name}
+												weight={dish.weight}
+												price={dish.price}
+												about={dish.about}
+											/>
 										</SwiperSlide>
 									);
 								})}
@@ -71,3 +86,4 @@ const Content = ({setActiveSection}: IactiveSection) => {
 };
 
 export default Content;
+
