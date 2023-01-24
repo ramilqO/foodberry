c84e935bb665ea4c1395bd1e81f49b871fac0870
MaximProsin <maximprosin647@gmail.com>
2023-01-24 13:03:02 +0300

    promotion card starting, promotion database beta, promotions block

---
+++ src/pages/Main/Promotions/Promotions.tsx
@@ -0,0 +1,17 @@
+import "./Promotions.scss";
+import promotion from "./promotionsData";
+
+import PromotionCard from '../../../components/PromotionCard/PromotionCard';
+
+const Promotions = () => {
+    console.log(promotion);
+    return (
+        <section className="1">
+            <div className="2">
+                
+            </div>
+        </section>
+    );
+}
+
+export default Promotions;
