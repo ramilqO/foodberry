8d09bc6be1fd5b9413f7a8bef3e10e7f9bf7b468
MaximProsin <maximprosin647@gmail.com>
2023-01-29 12:50:59 +0300

    all pravki

--- src/pages/Main/Promotions/Promotions.tsx
+++ src/pages/Main/Promotions/Promotions.tsx
@@ -7,20 +7,22 @@ const Promotions = () => {
     return (
         <main className="main">
             <section className="promotions">
-                <div className="promotions__title-wrapper">
-                    <h1 className="promotions__title">Акции</h1>
-                </div>
                 <div className="promotions__container">
-                    {promotion.map((item, index) => {
-                        return (
-                            <PromotionCard
-                                img={item.img}
-                                title={item.title}
-                                description={item.description}
-                                dateTime={item.dateTime}
-                            />
-                        );
-                    })}
+                    <div className="promotions__title-wrapper">
+                        <h1 className="promotions__title">Акции</h1>
+                    </div>
+                    <div className="promotions__promotion-cards">
+                        {promotion.map((item, index) => {
+                            return (
+                                <PromotionCard
+                                    img={item.img}
+                                    title={item.title}
+                                    description={item.description}
+                                    dateTime={item.dateTime}
+                                />
+                            );
+                        })}
+                    </div>
                 </div>
             </section>
         </main>
