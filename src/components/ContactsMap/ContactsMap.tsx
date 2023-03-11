ce8c9533cccce6349e9ba07e48640efe8c30d11e
Andrew Chernigovsky <chernigovsky108@gmail.com>
2023-03-11 15:45:23 +0300

    адатив и полное завершение footer && contactsMap

--- src/components/ContactsMap/ContactsMap.tsx
+++ src/components/ContactsMap/ContactsMap.tsx
@@ -5,43 +5,34 @@ const ContactsMap = () => {
 		<>
 			<div className="contactsMap">
 				<h2 className="contactsMap__title">контакты</h2>
-				<div className="contactsMap__description" onClick={(e: React.MouseEvent<HTMLHeadingElement>) => e.currentTarget.style.height = 'max-content'} onDoubleClick={(e: React.MouseEvent<HTMLHeadingElement>) => e.currentTarget.style.height = '150px'}>
+				<div className="contactsMap__description">
 					<div className="contactsMap__adress">
 						<span>Наш адрес:</span>
 						<p> МО, городской округ Красногорск, село Ильинкое, Экспериментальная улица, 10</p>
 					</div>
 					<div className="contactsMap__emails">
-					<a href='mailto:chernigovsky108@gmail.com' className="contactsMap__email">
-						<span>Максим (frontend-developer):</span>
-						<p>chernigovsky108@gmail.com</p>
-					</a>
-
 						<a href='mailto:chernigovsky108@gmail.com' className="contactsMap__email">
-							<span>Андрей (frontend-developer):</span>
-							<p>chernigovsky108@gmail.com</p>
-						</a>
-						<a href='mailto:chernigovsky108@gmail.com' className="contactsMap__email">
-							<span>Рамиль (frontend-developer):</span>
-							<p>chernigovsky108@gmail.com</p>
-						</a>
-						<a href='mailto:chernigovsky108@gmail.com' className="contactsMap__email">
-							<span>Семён (frontend-developer && team leader):</span>
-							<p>chernigovsky108@gmail.com</p>
+							<span>Наша почта:</span>
+							<p>auto.wash@gmail.com</p>
 						</a>
 					</div>
 
 				</div>
 
 				<div className="contactsMap__contacts">
-					<button type="button" disabled>свяжитесь с нами</button>
-					<p className="contactsMap__telephone"><a href="tel:+79999999999">+7 (999) 999-99-99</a><span>Звоните или оставляйте заявку</span></p>
+					<button type="button">ЗАБРОНИРОВАТЬ СТОЛ</button>
+					<p className="contactsMap__telephone"><a href="tel:+79999999999">+7 (917) 510-57-59</a><span>Звоните или оставляйте заявку</span></p>
 				</div>
-
-				<div className="contactsMap__githubs">
-					<a href="https://github.com/MaximProsin">М</a>
-					<a href="https://github.com/AndrewChernigovsky">А</a>
-					<a href="https://github.com/ramilqO">Р</a>
-					<a href="https://github.com/GitStudentSem">С</a>
+				<div className="contactsMap__social-media">
+					<div className="social-media">
+						<p>Мы в соц сетях:</p>
+						<div className="social-media-icons">
+							<a href="#" aria-label='facebook'></a>
+							<a href="#" aria-label='vkontakte'></a>
+							<a href="#" aria-label='youtube'></a>
+							<a href="#" aria-label='instagram'></a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
